8e9b376ea539494c65c7225ef0e3312f21865347
juanmi <juanmiguel@existo.es>
2018-08-23 13:27:00 +0200

    Formulario de proyectos y colecciones

--- routes/front.js
+++ routes/front.js
@@ -2,16 +2,31 @@ const express = require('express')
 const router = express.Router()
 
 // Formulario alta nuevo proyecto
-router.get ('/proyecto', function (req, res, next) {
+router.get ('/', function (req, res, next) {
 
-	res.render ('layout');
+	res.render ('home');
 });
+
+router.get ('/proyecto', (req,res,next) => {
+
+	res.render ('proyecto');
+});
+
+router.get ('/coleccion', (req,res,next) => {
+
+	res.render ('coleccion');
+});
+
+// router.get ('/proyecto', function (req, res, next) {
+
+// 	res.render ('layout');
+// });
 
 // Formulario alta nueva colección
-router.get ('/proyecto', function (req, res, next) {
+// router.get ('/proyecto', function (req, res, next) {
 
-	res.render ('layout');
-});
+// 	res.render ('layout');
+// });
 
 
 module.exports = router
